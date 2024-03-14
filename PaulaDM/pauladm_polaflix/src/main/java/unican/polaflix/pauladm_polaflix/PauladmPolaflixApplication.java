146dfae21a33c5cb016de15968a70213166d31be
pdm858 <paauulaadoomiingueezz@gmail.com>
2024-03-14 11:33:54 +0100

    ProyectoSpring
    Creación del proyecto Spring para el comienzo del proyecto PolaFlix.

---
+++ PaulaDM/pauladm_polaflix/src/main/java/unican/polaflix/pauladm_polaflix/PauladmPolaflixApplication.java
@@ -0,0 +1,13 @@
+package unican.polaflix.pauladm_polaflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauladmPolaflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauladmPolaflixApplication.class, args);
+	}
+
+}
---
+++ PaulaDM/pauladm_polaflix/src/test/java/unican/polaflix/pauladm_polaflix/PauladmPolaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package unican.polaflix.pauladm_polaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauladmPolaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
